a696fd1c65d920c85d4f75909146d178905df375
Nitin saini <nsaini0904@gmail.com>
2022-07-30 18:12:18 +0530

    Done with creating the chat engine using chat socket. A bug still prevails where the send button doesn't work.

--- index.js
+++ index.js
@@ -15,6 +15,12 @@ const sassMiddleware = require('node-sass-middleware');
 const flash = require('connect-flash');
 const customMWare = require('./config/middleware');
 
+// setup the chat server to be used with socket.io
+const chatServer = require('http').Server(app);
+const chatSockets = require('./config/chats_sockets').chatSockets(chatServer);
+chatServer.listen(5000);
+console.log('Chat server is listening on port 5000');
+
 // SASS middleware
 app.use(sassMiddleware({
   src : './assets/scss',
